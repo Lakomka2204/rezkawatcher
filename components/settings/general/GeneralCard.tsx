75c2ca12b93b432cd17b318c1735eb2fa2a784ee
Lakomka2204 <denis.kozachok22@gmail.com>
2024-03-11 14:04:41 +0200

    поменял галочки

--- components/settings/general/GeneralCard.tsx
+++ components/settings/general/GeneralCard.tsx
@@ -1,8 +1,8 @@
-import * as React from 'react';
 import { Text, View } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome6';
 import TouchableCard from '../../TouchableCard';
 import { Theme } from '@react-navigation/native';
+import { useMemo } from 'react';
 
 interface ThemeCardProps {
   value:string
@@ -13,19 +13,19 @@ interface ThemeCardProps {
 }
 
 function GeneralCard({onSelect,selected,value,text,theme}: ThemeCardProps) {
+  const isSelected = useMemo(() => {
+    return selected == value;
+  },[selected])
   return (
     <TouchableCard
       className='m-1 p-3 rounded-md'
       style={{ backgroundColor: theme.colors.card }}
       onClick={() => onSelect(value)}>
-      <View className='flex flex-row'>
-        <View className='flex flex-col flex-grow'>
+      <View className='flex flex-row items-center'>
+        <Icon color={isSelected ? 'green' :'gray'} name={isSelected?'circle-check':'circle'} solid={isSelected} size={24}/>
+        <View className='flex flex-col flex-grow mx-2'>
           <Text className='text-lg' style={{color: theme.colors.text}}>{text}</Text>
         </View>
-        {
-          selected == value &&
-        <Icon color={'green'} name='circle-check' solid size={24}/>
-        }
       </View>
     </TouchableCard>
   );
